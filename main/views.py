333b3f68aad461473ecc31df6ab9fc4190f1d000
Md. Manik Sarker <manikdae34@gmail.com>
2023-05-05 16:32:03 +0600

    static part added

--- main/views.py
+++ main/views.py
@@ -24,7 +24,7 @@ def main(request):
         u.save()
     countdata = Visitor.objects.all().count()
 
-    aboutdata = About.objects.all()
+    aboutdata = About.objects.all()[0]
     sliderdata = SliderItem.objects.all()
     academicdata = Academic.objects.all()
     experiencedata = Experience.objects.all()
